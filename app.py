46830246f538e67a73d806e7c0365cdff72439fe
Lewis N <lewis.ng@education.nsw.gov.au>
2026-06-16 00:15:22 +0000

    setup: initial Flask project structure

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return "<h1>Hello! My Website is Coming Soon!</h1>"
 
 
 if __name__ == "__main__":
